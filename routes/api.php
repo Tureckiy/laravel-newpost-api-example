73afb6134afa71a085f3aecba401943636e3dcc6
tureckiy <ras.kdn@gmail.com>
2023-04-13 01:52:20 +0300

    clear some comments

--- routes/api.php
+++ routes/api.php
@@ -20,11 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::get('/users', [UserController::class, 'getUsers']);
-//Route::get('/user', [UserController::class, 'getSinfgeUser']);
-
-//Route::post('/user/update/{id}', [UserController::class, 'store']);
-
 Route::apiResources([
     'users' => UserController::class
 ]);
